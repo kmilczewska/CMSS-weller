949230eb07ad08086f77d579fbdf25f89bb33f74
Kaja Milczewska <k.m.milczewska@pgr.reading.ac.uk>
2017-09-12 14:12:01 +0100

    First commit of burger_nl and functions

---
+++ code/functions.py
@@ -0,0 +1,37 @@
+## File of functions for FTBS and CTCS schemes.
+
+## Define the initial and analytic solution:
+def initial_bell(x):
+    return np.where(x%1. <0.5, np.power(np.sin(2*x*pi),2), 0)
+
+def FTBS(phi,phiNew):
+    """FTCS scheme for burgers equation """
+    ## Loop over all timestps using FTCS:
+    for n in xrange(nt):
+        ## Loop over space
+        for j in xrange(1,nx):
+    	    phiNew[j] = phi[j] - ((dt/dx)*phi[j])*(phi[j]-phi[j-1])
+        ## Setup boundary conditions
+        phiNew[0] = phi[0] - ((dt/dx)*phi[0])*(phi[0]-phi[nx-1])
+        phiNew[nx] = phiNew[0]
+
+        phi = phiNew.copy()
+        plot_solution(phi)
+
+    return phi
+
+
+def plot_solution(p):
+    """Function to plot the solution """
+    plt.figure(1)
+    #plt.clf()
+    
+    plt.plot(x, initial_bell(x - u*t), 'k', label='analytics')
+    plt.plot(x, p, 'b', label='FTBS')
+    #plt.legend(loc='best')
+    plt.xlabel('x')
+    plt.ylabel('$\phi$')
+    plt.axhline(0,linestyle=':',color='black')
+    plt.savefig('burgers.png')
+    plt.show()
+    plt.pause(0.01)
